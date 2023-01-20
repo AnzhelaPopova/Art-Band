e8529412f8e87f1a4bf02559f79ecd4b4fbfeba8
AnzhelaPopova <new.daisy@mail.ru>
2023-01-20 13:05:27 +0300

    delete ignor dist

---
+++ design/assets/dist/js/main.js
@@ -0,0 +1,108 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nError: Can't resolve './layouts/diary.css' in 'C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\src\\\\scss'\\n    at finishWithoutResolve (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:309:18)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:386:15\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:16:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:27:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\DescriptionFilePlugin.js:87:43\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at tryRunOrWebpackError (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5058:12)\\n    at __webpack_require__ (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5015:18)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5086:20\\n    at symbolIterator (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4993:43\\n    at symbolIterator (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nError: Can't resolve './layouts/diary.css' in 'C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\src\\\\scss'\\n    at finishWithoutResolve (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:309:18)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:386:15\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:16:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:27:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\DescriptionFilePlugin.js:87:43\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at eval (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\enhanced-resolve\\\\lib\\\\Resolver.js:435:5\\n    at Object.<anonymous> (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\src\\\\scss\\\\app.scss:1:7)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5060:39\\n    at tryRunOrWebpackError (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5058:12)\\n    at __webpack_require__ (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5015:18)\\n    at C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5086:20\\n    at symbolIterator (C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\DC\\\\Desktop\\\\IT_Girls\\\\project\\\\Art-Band\\\\src\\\\scss\\\\app.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\\\nError: Can't resolve './layouts/diary.css' in 'C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\src\\\\\\\\scss'\\\\n    at finishWithoutResolve (C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:309:18)\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:386:15\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:435:5\\\\n    at eval (eval at create (C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\tapable\\\\\\\\lib\\\\\\\\HookCodeFactory.js:33:10), <anonymous>:16:1)\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:435:5\\\\n    at eval (eval at create (C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\tapable\\\\\\\\lib\\\\\\\\HookCodeFactory.js:33:10), <anonymous>:27:1)\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\DescriptionFilePlugin.js:87:43\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:435:5\\\\n    at eval (eval at create (C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\tapable\\\\\\\\lib\\\\\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\\\n    at C:\\\\\\\\Users\\\\\\\\DC\\\\\\\\Desktop\\\\\\\\IT_Girls\\\\\\\\project\\\\\\\\Art-Band\\\\\\\\node_modules\\\\\\\\enhanced-resolve\\\\\\\\lib\\\\\\\\Resolver.js:435:5\\\");\");\n\n//# sourceURL=webpack://design/./src/scss/app.scss?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/app.scss */ \"./src/scss/app.scss\");\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_app_scss__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\n\n//# sourceURL=webpack://design/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
