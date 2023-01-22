40b87da55d3be9bfb9f0003e337343512cf34640
Inna <melikhovainna@gmail.com>
2023-01-22 11:59:05 +0300

    trying to renew main.css

--- design/assets/dist/js/main.js
+++ design/assets/dist/js/main.js
@@ -7,16 +7,15 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/scss/app.scss":
 /*!***************************!*\
   !*** ./src/scss/app.scss ***!
   \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://design/./src/scss/app.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: \\\"+\\\" and \\\"-\\\" must be surrounded by whitespace in calculations.\\n   ╷\\n75 │ @media screen and (min-width: calc($tablet+1px)) {\\r\\n   │                                           ^\\n   ╵\\n  src\\\\scss\\\\pages\\\\mk4.scss 75:43  @import\\n  src\\\\scss\\\\app.scss 12:9         root stylesheet\\n    at tryRunOrWebpackError (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5058:12)\\n    at __webpack_require__ (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5015:18)\\n    at E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5086:20\\n    at symbolIterator (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4993:43\\n    at symbolIterator (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: \\\"+\\\" and \\\"-\\\" must be surrounded by whitespace in calculations.\\n   ╷\\n75 │ @media screen and (min-width: calc($tablet+1px)) {\\r\\n   │                                           ^\\n   ╵\\n  src\\\\scss\\\\pages\\\\mk4.scss 75:43  @import\\n  src\\\\scss\\\\app.scss 12:9         root stylesheet\\n    at Object.<anonymous> (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\src\\\\scss\\\\app.scss:1:7)\\n    at E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5060:39\\n    at tryRunOrWebpackError (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5058:12)\\n    at __webpack_require__ (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5015:18)\\n    at E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5086:20\\n    at symbolIterator (E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\lessons\\\\Art-Band\\\\src\\\\scss\\\\app.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nSassError: \\\\\\\"+\\\\\\\" and \\\\\\\"-\\\\\\\" must be surrounded by whitespace in calculations.\\\\n   ╷\\\\n75 │ @media screen and (min-width: calc($tablet+1px)) {\\\\r\\\\n   │                                           ^\\\\n   ╵\\\\n  src\\\\\\\\scss\\\\\\\\pages\\\\\\\\mk4.scss 75:43  @import\\\\n  src\\\\\\\\scss\\\\\\\\app.scss 12:9         root stylesheet\\\");\");\n\n//# sourceURL=webpack://design/./src/scss/app.scss?");
 
 /***/ }),
 
@@ -26,7 +25,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/app.scss */ \"./src/scss/app.scss\");\n\r\n\n\n//# sourceURL=webpack://design/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/app.scss */ \"./src/scss/app.scss\");\n\n\n\n//# sourceURL=webpack://design/./src/index.js?");
 
 /***/ })
 
